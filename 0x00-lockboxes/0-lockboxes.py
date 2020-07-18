ad927c49c300afea170c660ad534010d953d9ae2
ilasso <1204@holbertonschool.com>
2020-07-18 17:46:29 -0500

    first version

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    return True
+    dictboxes = {0: 'open'}
+    dictboxes2 = {}
+
+    # iteration all boxes
+    for i in boxes:
+        count = 0  # open boxes
+        # add open box for each position
+        for j in i:
+            # verify if box is was or could be open
+            # if there any key, add how an open box in a dictboxes
+            # and incremente counter of opened boxes
+            if j not in dictboxes:
+                dictboxes[j] = 'open'
+                count = count + 1
+                # if dont have to add a opened box return false
+                if count == 0:
+                    return False
+        # checks if each box opened have keys to open more boxes
+        for k, l in dictboxes.items():
+            for m in boxes[k]:
+                if m not in dictboxes:
+                    dictboxes2[m] = 'open'
+        # insert box apened into dictboxes
+        for n, o in dictboxes2.items():
+            if n not in dictboxes:
+                dictboxes[n] = o
+                count = count + 1
+        # if doesnt have more boxes to open return false
+        if count == 0:
+            return False
+        # if all boxes are opened return true
+        if (len(boxes) == len(dictboxes)) and count:
+            return True
+    print(dictboxes)
+    return False
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -10,3 +10,6 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[5], [1,3], [5,1], [2,3], [4,6], [4, 3,2], []]
+print(canUnlockAll(boxes))

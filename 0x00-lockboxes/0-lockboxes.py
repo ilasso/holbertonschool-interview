8ce6a768cee41de66b2d38ac6991b024aa3e1bfd
ilasso <1204@holbertonschool.com>
2020-07-19 01:23:56 -0500

    wrong pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """0-lockboxes.py: determines if all the boxes can be opened"""
 
+
 def canUnlockAll(boxes):
     """ determines if all the boxes can be opened"""
-    if len(boxes) == 0:
-        return False
-    if type(boxes) is not list:
-        return False
     if boxes == [[0]]:
         return True
     if boxes == []:
@@ -14,17 +11,14 @@ def canUnlockAll(boxes):
     if boxes == [[]]:
         return False
     # if boxes is not coherent
-    maxim = max([item for lista in boxes for item in lista])
-    if maxim != len(boxes) - 1:
+    if max(boxes)[0] != len(boxes) - 1:
         return False
-
     if [0] not in boxes:
         dictboxes = {0: 'open'}
     else:
         dictboxes = {}
     dictboxes2 = {}
-    if boxes[0] == [0]:
-        return False
+
     # iteration all boxes
     for i in boxes:
         count = 0  # open boxes
@@ -37,20 +31,22 @@ def canUnlockAll(boxes):
                 dictboxes[j] = 'open'
                 count = count + 1
                 # if dont have to add a opened box return false
-                #if count == 0:
-                #    return False
+                if count == 0:
+                    return False
         # checks if each box opened have keys to open more boxes
         for k, l in dictboxes.items():
             for m in boxes[k]:
                 if m not in dictboxes:
                     dictboxes2[m] = 'open'
-                    count = count + 1
         # insert box apened into dictboxes
         for n, o in dictboxes2.items():
             if n not in dictboxes:
                 dictboxes[n] = o
                 count = count + 1
+        # if doesnt have more boxes to open return false
+        if count == 0:
+            return False
         # if all boxes are opened return true
-        if (len(boxes) == len(dictboxes)):
+        if (len(boxes) == len(dictboxes)) and count:
             return True
     return False

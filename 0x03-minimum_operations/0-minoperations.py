3adc44736e6809f4f73080063326252f90efaa85
ilasso <1204@holbertonschool.com>
2020-08-05 23:47:12 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """
+minOperations: Function
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n,
+write a method that calculates the fewest number of operations needed
+to result in exactly n H characters
+
+Returns an integer
+If n is impossible to achieve, return 0
 """
+
+
 def minOperations(n):
     lista = []
     numberKey = 0
@@ -11,7 +21,3 @@ def minOperations(n):
     if lista:
         return (min(lista))
     return 0
-    #if numberKey == n:
-    #    return 0
-    #minOper = 2 + ((n / numberKey) - 2) + 1 + (numberKey - 1)
-    #return minOper

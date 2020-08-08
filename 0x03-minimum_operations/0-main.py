3da4df76f75a695554ff7e5a82ef389adb639072
ilasso <1204@holbertonschool.com>
2020-08-08 18:29:35 -0500

    validate n = 1 n = 0

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -5,6 +5,12 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
+n = 1 # 0
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 0 # 0
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
 n = 9 # 6
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 

abd9e98a125625bc17e3895e51137235a7392bbf
ilasso <1204@holbertonschool.com>
2020-08-30 14:48:39 -0500

    numeric

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -33,9 +33,9 @@ if __name__ == '__main__':
                 print("File size: {:d}".format(totalsize))
                 for i, j in sorted(dictstatus.items()):
                     if j != 0:
-                        print("{}: {}".format(i, j))
+                        print("{:d}: {:d}".format(int(i), int(j)))
     except KeyboardInterrupt:
         print("File size: {:d}".format(totalsize))
         for i, j in sorted(dictstatus.items()):
             if j != 0:
-                print("{}:{}".format(i, j))
+                print("{:d}:{:d}".format(int(i), int(j)))

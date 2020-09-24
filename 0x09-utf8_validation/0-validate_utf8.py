078d0f167e0bce9a6668d4b3f5d35186f41a7266
ilasso <1204@holbertonschool.com>
2020-09-23 19:22:45 -0500

    pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
+
 def validUTF8(data):
     cnt = 0
     for x in data:
         x = bin(x)[2:].zfill(8)
-        if cnt: # in the middle of multi-byte
-            if x.startswith("10"): cnt -= 1
-            else: return False
-        else: # beginning
+        if cnt:
+            if x.startswith("10"):
+                cnt -= 1
+            else:
+                return False
+        else:
             cnt = x.find("0")
-            if cnt == -1 or cnt == 1 or cnt > 4: return False
-            if cnt: cnt -= 1
+            if cnt == -1 or cnt == 1 or cnt > 4:
+                return False
+            if cnt:
+                cnt -= 1
     return cnt == 0

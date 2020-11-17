1f4a5550c324fab3ea8b2acc6c572b3cb4e604b5
ilasso <1204@holbertonschool.com>
2020-11-17 15:57:43 -0500

    semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 
-/* consumes films by argument*/
+/* consumes films by argument */
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, { json: true }, (err, res, body) => {
-	let index = 0
-	if (err) { return console.log(err); }
-	fnCharacters(body.characters, 0);
+  if (err) { return console.log(err); }
+  fnCharacters(body.characters, 0);
 });
 /* function to get characters by film */
-/* invokes recursively(SYNC) to execute in order NOASYNC*/
-const fnCharacters = function GetCharacters(arrayurl, index) {
-	if (arrayurl.length === index) return;
-	request(arrayurl[index], { json: true }, (err, res, body) => {
-		if (err) { return console.log(err); }
-		console.log(body.name);
-		fnCharacters(arrayurl, index + 1);
-	});
-}
+/* invokes recursively(SYNC) to execute in order NOASYNC */
+const fnCharacters = function GetCharacters (arrayurl, index) {
+  if (arrayurl.length === index) return;
+  request(arrayurl[index], { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(body.name);
+    fnCharacters(arrayurl, index + 1);
+  });
+};
